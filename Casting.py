45542160e3613cd6e65bb9e9bd77cc908e1995a6
Kalilaaa <kalilah_arafah_33rpl@student.smktelkom-mlg.sch.id>
2025-10-20 14:01:09 +0700

    Add files via upload

---
+++ Casting.py
@@ -0,0 +1,77 @@
+# # cast from str to int
+# str_numbers = "456"
+# str_numbers_to_int = int(str_numbers)
+# print("Before casting :", str_numbers, ", the data type is",
+# type(str_numbers))
+# print("After casting :", str_numbers_to_int, ", the data type is",
+# type(str_numbers_to_int))
+
+
+# # casting from int to str
+# integer = 12345
+# integer_to_str = str(integer)
+# print("Before casting :", integer, ", the data type is", type(integer))
+# print("After casting :", integer_to_str, ", the data type is",
+# type(integer_to_str))
+
+
+# # casting from int to bool
+# num_int = 1
+# num_bool = bool(num_int)
+# print(num_bool, type(num_bool))
+# num_int = 0
+
+# num_bool = bool(num_int)
+# print(num_bool, type(num_bool))
+
+
+# # Equal to
+# 8 == 8
+# # Not equal to
+# 8 != 9
+# # Greater than
+# 8 > 9
+# # Less than
+# 8 < 9
+# # Less than
+# 8 <= 9
+# # Less than
+# 9 >= 9
+
+
+# a = True
+# b = True
+# print(a and b)
+# print(a or b)
+# print(not b)
+# #logic
+# 5 > 6 and 6 < 7
+
+
+# e = 8
+# f = 2
+# # Summation
+# sum = e + f
+# print(f"The sum of e with f is : {sum}\n")
+# # Reduction
+# red = e - f
+# print(f"The reduction of e with f is : {red}\n")
+
+# # Multiplication
+# multi = e * f
+# print(f"The multipication of e with f is : {multi}\n")
+# # Division
+# divi = e / f
+# print(f"The quotient of e with f is : {divi}\n")
+# # Modulo
+# mod = e % f
+# print(f"The remainder of e with f is : {mod}\n")
+# # Power
+# pow = e ** f
+# print(f"The power of e of f is : {pow}\n")
+
+
+# name = str(input("What is your name : "))
+# age = int(input("What's your age : "))
+# print("Name: ", name)
+# print("Age: ", age)
